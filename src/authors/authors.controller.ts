33827cf553ca62c274bc417d54a3e0ede250dd9f
dave8git <dave8git@gmail.com>
2024-12-19 22:31:08 +0100

    Add guards added

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -1,46 +1,50 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, ParseUUIDPipe, Post, Put } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param, ParseUUIDPipe, Post, Body, Put, Delete, UseGuards } from '@nestjs/common';
 import { AuthorsService } from './authors.service';
 import { CreateAuthorDTO } from './dtos/create-author.dto';
 import { UpdateAuthorDTO } from './dtos/update-author.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('authors')
 export class AuthorsController {
-  constructor(private authorsService: AuthorsService) { }
+    constructor(private authorsService: AuthorsService) {}
 
-  @Get('/')
-  getAll() {
-    return this.authorsService.getAll();
-  }
+    @Get('/')
+    getAll(): any {
+        return this.authorsService.getAll();
+    }
 
-  @Get('/:id')
-  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
-    const author = await this.authorsService.getById(id);
-    if (!author) throw new NotFoundException('Author not found');
-    return author;
-  }
+    @Get('/:id')
+    async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+        const author = await this.authorsService.getById(id);
+        if (!author) throw new NotFoundException('Author not found');
+        return author;
+    }
 
-  @Post('/')
-  create(@Body() authorData: CreateAuthorDTO) {
-    return this.authorsService.create(authorData);
-  }
+    @Post('/')
+    @UseGuards(JwtAuthGuard)
+    create(@Body() authorData: CreateAuthorDTO) {
+        return this.authorsService.create(authorData);
+    }
 
-  @Put('/:id')
-  async update(
-    @Param('id', new ParseUUIDPipe()) id: string,
-    @Body() authorData: UpdateAuthorDTO,
-  ) {
+    @Put('/:id')
+    @UseGuards(JwtAuthGuard)
+    async update(
+      @Param('id', new ParseUUIDPipe()) id: string,
+      @Body() authorData: UpdateAuthorDTO,
+    ) {
+      if (!(await this.authorsService.getById(id)))
+        throw new NotFoundException('Author not found');
+
+      await this.authorsService.updateById(id, authorData);
+      return { success: true };
+    }
+
+    @Delete('/:id')
+    @UseGuards(JwtAuthGuard)
+    async deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
     if (!(await this.authorsService.getById(id)))
-      throw new NotFoundException('Author not found');
-
-    await this.authorsService.updateById(id, authorData);
-    return { success: true };
-  }
-
-  @Delete('/:id')
-  async deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
-    if (!(await this.authorsService.getById(id)))
-      throw new NotFoundException('Author not found');
+        throw new NotFoundException('Author not found');
     await this.authorsService.deleteById(id);
     return { success: true };
-  }
+    }
 }
--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,46 +1,50 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, ParseUUIDPipe, Post, Put } from '@nestjs/common';
+import { Controller, Get, Param, NotFoundException, Post, Body, Put, Delete, UseGuards } from '@nestjs/common';
 import { BooksService } from './books.service';
+import { ParseUUIDPipe } from '@nestjs/common';
 import { CreateBookDTO } from './dtos/create-book.dto';
 import { UpdateBookDTO } from './dtos/update-book.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('books')
 export class BooksController {
-  constructor(private booksService: BooksService) { }
+    constructor(private booksService: BooksService) {}
 
-  @Get('/')
-  getAll() {
-    return this.booksService.getAll();
-  }
+    @Get('/')
+    getAll() {
+        return this.booksService.getAll();
+    }
 
-  @Get('/:id')
-  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
-    const book = await this.booksService.getById(id);
-    if (!book) throw new NotFoundException('Book not found');
-    return book;
-  }
+    @Get('/:id')
+    async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+        const book = await this.booksService.getById(id);
+        if (!book) throw new NotFoundException('Book not found');
+        return book;
+    }
 
-  @Post('/')
-  create(@Body() bookData: CreateBookDTO) {
-    return this.booksService.create(bookData);
-  }
+    @Post('/')
+    @UseGuards(JwtAuthGuard)
+    public create(@Body() bookData: CreateBookDTO) {
+        return this.booksService.create(bookData);
+    }
 
-  @Put('/:id')
-  async update(
-    @Param('id', new ParseUUIDPipe()) id: string,
-    @Body() bookData: UpdateBookDTO,
-  ) {
-    if (!(await this.booksService.getById(id)))
-      throw new NotFoundException('book not found');
+    @Put('/:id')
+    @UseGuards(JwtAuthGuard)
+    async updateById(
+        @Param('id', new ParseUUIDPipe()) id: string,
+        @Body() bookData: UpdateBookDTO,
+    ) {
+        if (!(await this.booksService.getById(id)))
+        throw new NotFoundException('Book not found');
+        await this.booksService.updateById(id, bookData);
+        return { success: true };
+    }
 
-    await this.booksService.updateById(id, bookData);
-    return { success: true };
-  }
-
-  @Delete('/:id')
-  async deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
+    @Delete('/:id')
+    @UseGuards(JwtAuthGuard)
+    async delete(@Param('id', new ParseUUIDPipe()) id: string) {
     if (!(await this.booksService.getById(id)))
       throw new NotFoundException('Book not found');
     await this.booksService.deleteById(id);
     return { success: true };
-  }
+    }
 }
